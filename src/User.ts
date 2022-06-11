24289ddfc49821041eae2e9eed22d266e8db8e06
Carson <carsonlneal@gmail.com>
2022-06-11 19:42:40 -0400

    adding src

---
+++ src/User.ts
@@ -0,0 +1,25 @@
+import faker from 'faker';
+import { Mappable } from './CustomMap';
+// In the future, use faker-js instead since it is being actively supported
+
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'red';
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
+    };
+  }
+
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
+
+}
